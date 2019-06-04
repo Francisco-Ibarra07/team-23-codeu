a7a9cec3b627e66f14a555c57d552d1365b4b878
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-06-03 22:54:39 -0700

    Updated servlet to use updated Datastore to fetch and store a user's AboutMe info

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -10,54 +10,62 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Message;
+import com.google.codeu.data.User;
+
 /**
- * Responds with a hard-coded message for testing purposes.
+ * Handles fetching and saving user data.
  */
 @WebServlet("/about")
-public class AboutMeServlet extends HttpServlet{
+public class AboutMeServlet extends HttpServlet {
 
-    private Datastore datastore;
+  private Datastore datastore;
 
-    @Override
-    public void init() {
-        datastore = new Datastore();
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+   * Responds with the "about me" section for a particular user.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    response.setContentType("text/html");
+
+    String user = request.getParameter("user");
+
+    if(user == null || user.equals("")) {
+      // Request is invalid, return empty response
+      return;
     }
 
-    /**
-     * Responds with the "about me" section for a particular user
-     */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
-        // Sets the body type to HTML
-        response.setContentType("text/html");
+    User userData = datastore.getUser(user);
 
-        String user = request.getParameter("user");
-
-        // Request is invalid, return an empty response
-        if(user == null || user.equals("")) {
-            return;
-        }
-
-        // If the user is valid, print out a short sentence with their name
-        String aboutMe = "This is " + user + "'s about me.";
-        response.getOutputStream().println(aboutMe);
+    if(userData == null || userData.getAboutMe() == null) {
+      return;
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.getOutputStream().println(userData.getAboutMe());
+  }
 
-        UserService userService = UserServiceFactory.getUserService();
-        if (!userService.isUserLoggedIn()) {
-            response.sendRedirect("/index.html");
-            return;
-        }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
 
-        String userEmail = userService.getCurrentUser().getEmail();
-        System.out.println("Saving about me for " + userEmail);
-        // TODO: save the data
-
-        response.sendRedirect("/user-page.html?user=" + userEmail);
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
     }
+
+    String userEmail = userService.getCurrentUser().getEmail();
+    String aboutMe = request.getParameter("about-me");
+
+    User user = new User(userEmail, aboutMe);
+    datastore.storeUser(user);
+
+    response.sendRedirect("/user-page.html?user=" + userEmail);
+  }
 }
