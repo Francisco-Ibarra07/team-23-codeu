8220eb2453720f7923882f616700993534910251
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-06-14 14:36:51 -0700

    Updated MessageServlet with quick comments

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -78,9 +78,11 @@ public class MessageServlet extends HttpServlet {
     String user = userService.getCurrentUser().getEmail();
     String userText = Jsoup.clean(request.getParameter("text"), Whitelist.none());
 
+    // Regexes to detect images or video links
     String imageRegex = "(https?://\\S+\\.(png|jpg|gif))";
     String videoRegex = "(https?://\\S+\\.(mp4|webm|ogg|3gp))";
 
+    // If a link is found, this is what it would be replaced with
     String imageReplacement = "<img src=\"$1\" alt=\"Couldn't load image\" />";
     String videoReplacement = "\n<video width=\"320\" height=\"240\" controls>\n" + 
                               "<source src=\"$1\" type=\"video/mp4\">\n" +
@@ -90,6 +92,7 @@ public class MessageServlet extends HttpServlet {
     String newText = userText.replaceAll(imageRegex, imageReplacement); 
     newText = userText.replaceAll(videoRegex, videoReplacement);        
   
+    // Store new text to the database
     Message message = new Message(user, newText);
     datastore.storeMessage(message);
 
