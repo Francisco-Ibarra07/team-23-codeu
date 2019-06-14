aa96d09bbdbda41d0b1226209dc12bfbf1ea7646
abisolaOlawale <abbyh.olawale@yahoo.com>
2019-06-13 19:33:48 -0700

    added logic to get list of users in datastore class

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -26,6 +26,7 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.*; 
 
 /** Provides access to the data stored in Datastore. */
 public class Datastore {
@@ -114,5 +115,15 @@ public class Datastore {
     }
 
     return messages;
+  }
+
+  public Set<String> getUsers(){
+    Set<String> users = new HashSet<>();
+    Query query = new Query("Message");
+    PreparedQuery results = datastore.prepare(query);
+    for(Entity entity : results.asIterable()) {
+      users.add((String) entity.getProperty("user"));
+    }
+    return users;
   }
 }
