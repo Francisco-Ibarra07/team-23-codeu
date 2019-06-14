5ed0c1e64f454df2bc70c6968c90e2652deb0e2c
Elizabeth <elin6@uw.edu>
2019-06-13 17:47:10 -0700

    add fetch option

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -15,7 +15,7 @@
  */
 
 package com.google.codeu.data;
-
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
