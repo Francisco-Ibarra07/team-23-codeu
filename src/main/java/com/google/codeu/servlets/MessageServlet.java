2addef3320bb5c78cd12d88cf57e5b95c9b92395
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-06-22 23:04:21 -0700

    Added a regex to detect an image caption format

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -23,6 +23,8 @@ import com.google.codeu.data.Message;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Pattern;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -78,24 +80,41 @@ public class MessageServlet extends HttpServlet {
     String user = userService.getCurrentUser().getEmail();
     String userText = Jsoup.clean(request.getParameter("text"), Whitelist.none());
 
-    // Regexes to detect images or video links
-    String imageRegex = "(https?://\\S+\\.(png|jpg|gif))";
-    String videoRegex = "(https?://\\S+\\.(mp4|webm|ogg|3gp))";
-
-    // If a link is found, this is what it would be replaced with
-    String imageReplacement = "<img src=\"$1\" alt=\"Couldn't load image\" />";
-    String videoReplacement = "\n<video width=\"320\" height=\"240\" controls loop>\n" + 
-                              "<source src=\"$1\" type=\"video/mp4\">\n" +
-                              "</video>";
-
-    // Replace any image or video links as <img> tags so they can be rendered on a website                      
-    String newText = userText.replaceAll(imageRegex, imageReplacement); 
-    newText = newText.replaceAll(videoRegex, videoReplacement);        
+    // Utilize helper method to replace any image links into html tags
+    String newText = detectAndReplaceMediaLinks(userText);
   
     // Store new text to the database
     Message message = new Message(user, newText);
     datastore.storeMessage(message);
 
     response.sendRedirect("/user-page.html?user=" + user);
+  }
+
+  public String detectAndReplaceMediaLinks(String userText) {
+
+    String updatedText;
+
+    // Regexes to detect image links, video links, or image caption formats like ![caption here](/url/of/image.jpg)
+    String imageRegex = "(https?://\\S+\\.(png|jpg|gif))";
+    String videoRegex = "(https?://\\S+\\.(mp4|webm|ogg|3gp))";
+    String imageCaptionFormatRegex = "!{1}\\[{1}(.+)]{1}\\({1}(https?://\\S+\\.(png|jpg|gif))\\){1}";
+
+    // If a link is found, this is what it would be replaced with
+    String imageReplacement = "<img src=\"$1\" alt=\"Couldn't load image\" />";
+    String imageCaptionReplacement = "<figure>" + "<img src=\"$2\" alt=\"Couldn't load image\" />" + "<figcaption>$1</figcaption>" + "</figure>";
+    String videoReplacement = "<video width=\"320\" height=\"240\" controls loop>" + "<source src=\"$1\" type=\"video/mp4\">\n" + "</video>";
+  
+    // Check to see if the text contains a ![Caption Text](http//LinkToImage.jpg)
+    Boolean captionFormatIsFound = Pattern.compile(imageCaptionFormatRegex).matcher(userText).find();
+    if(captionFormatIsFound) {
+      updatedText = userText.replaceAll(imageCaptionFormatRegex, imageCaptionReplacement);
+    }
+    else {
+      // Replace any image or video links as <img> tags so they can be rendered on a website                      
+      updatedText = userText.replaceAll(imageRegex, imageReplacement); 
+      updatedText = updatedText.replaceAll(videoRegex, videoReplacement);        
+    }
+
+    return updatedText;
   }
 }
