2eedefa549c00eb5ff7d15e57f4925e5890eaf5c
abisolaOlawale <abbyh.olawale@yahoo.com>
2019-06-30 16:06:37 -0700

    updated servlet to parse data from csv file and return data as response

--- src/main/java/com/google/codeu/servlets/ChartServlet.java
+++ src/main/java/com/google/codeu/servlets/ChartServlet.java
@@ -1,39 +1,60 @@
 package com.google.codeu.servlets;
 
+
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Message;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import java.util.Scanner;
 
-/**
- * Handles fetching all messages for the public feed.
- */
+
 @WebServlet("/bookchart")
-public class ChartServlet extends HttpServlet{
-  
- private Datastore datastore;
+public class ChartServlet extends HttpServlet {
 
- @Override
+
+  private JsonArray bookRatingArray;
+
+  // create file to help with serializing the data
+  private static class bookRating {
+    String title;
+    double rating;
+
+    private bookRating (String title, double rating) {
+      this.title = title;
+      this.rating = rating;
+    }
+  }
+
+
+  @Override
+  // on start, read book-data.csv and add book title and rating to array of 
+  // book ratings 
   public void init() {
-    datastore = new Datastore();
-  }
- 
- /**
-  * Responds with a JSON representation of Message data for all users.
-  */
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    bookRatingArray = new JsonArray();
+    Gson gson = new Gson();
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/book-ratings.csv"));
+    scanner.nextLine(); //skips first line (the csv header)
+    while(scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
 
-    // Set the type of content that will be returned to 'json' format instead of html
-    response.setContentType("application/json");
-    response.getWriter().println("this is to test everything works"); 
-    
+      String curTitle = cells[5];
+      double curRating = Double.parseDouble(cells[6]);
+
+      bookRatingArray.add(gson.toJsonTree(new bookRating(curTitle, curRating)));
+    }
+    scanner.close();
   }
+
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+     response.setContentType("application/json");
+     response.getOutputStream().println(bookRatingArray.toString());
+   }
+
+
 }
