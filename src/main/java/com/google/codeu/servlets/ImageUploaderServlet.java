901c1aac6260aece286e3e4d0284f1bc06bb40be
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-06-29 21:05:52 -0700

    Added documentation to the ImageUploader Servlet file

--- src/main/java/com/google/codeu/servlets/ImageUploaderServlet.java
+++ src/main/java/com/google/codeu/servlets/ImageUploaderServlet.java
@@ -8,6 +8,8 @@ import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
 import com.google.appengine.api.images.ServingUrlOptions;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,16 +22,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * This servlet prints out the HTML for the homepage. You wouldn't do this in a real codebase,
- * but this is meant to demonstrate getting a Blobstore URL and using it in a form to allow
- * a user to upload a file.
+ * This servlet has two jobs:
+ *  1) Takes a user's image upload and sends it to Blobstore so it can store it somewhere.
+ *  2) Recieves the URL link of where Blobstore stored the image and gives this new link to the MessageServlet 
+ *     so it can create a new message on the user-page.
  */
 @WebServlet("/image-uploader-form")
 public class ImageUploaderServlet extends HttpServlet {
 
   /**
-   * Returns HTML that contains a form. The form submits to Blobstore,
-   * which redirects to our /my-form-handler, which is handled by FormHandlerServlet.
+   * Returns HTML that contains a form for a user to fill in a description and to upload an image from their computer. 
+   * The uploaded image is given to Blobstore so it can store it somewhere and return a url link to the new stored image.
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -38,24 +41,27 @@ public class ImageUploaderServlet extends HttpServlet {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     String uploadUrl = blobstoreService.createUploadUrl("/image-uploader-form") ;
 
+    // Set the response to be in HTML format
     response.setContentType("text/html");
 
     ServletOutputStream out = response.getOutputStream();
-    out.println("<form method=\"POST\" enctype=\"multipart/form-data\" action=\"" + uploadUrl + "\">");
-    System.out.println("Upload url in doGet: " + uploadUrl);
 
+    // Create a <form> where the user can input a text description and have a button to do a image file upload
+    out.println("<form method=\"POST\" enctype=\"multipart/form-data\" action=\"" + uploadUrl + "\">");
     out.println("<p>Type in a description:</p>");
     out.println("<textarea name=\"message\"></textarea>");
     out.println("<br/>");
-
     out.println("<p>Upload the image:</p>");
     out.println("<input type=\"file\" name=\"image\">");
     out.println("<br/><br/>");
-
     out.println("<button>Submit</button>");
     out.println("</form>");
   }
 
+  /**
+   * Obtains the URL link of the new uploaded image from Blobstore. Also puts together the user's description and the 
+   * URL link together into one text message and sends this to the MessageServlet.
+   */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -64,15 +70,23 @@ public class ImageUploaderServlet extends HttpServlet {
 
     // Get the URL of the image that the user uploaded to Blobstore.
     String imageUrl = getUploadedFileUrl(request, "image");
-    System.out.println("IMAGE URL: " + imageUrl);
+    if(imageUrl == null) {
+      // Send the user back to their user page if no file was uploaded
+      UserService userService = UserServiceFactory.getUserService();
+      String user = userService.getCurrentUser().getEmail();
+      response.sendRedirect("/user-page.html?user=" + user);
+      return;
+    }
 
+    // Construct the text message that will be sent to the MessageServlet
     String fullMessage = message + " " + imageUrl;
 
-    // Output some HTML that shows the data the user entered.
-    // A real codebase would probably store these in Datastore.
     response.setContentType("text/html");
     ServletOutputStream out = response.getOutputStream();
     
+    // Output some HTML that shows the data the user entered.
+    // Gives a user a 'Submit' button to confirm that the information is correct.
+    // Once 'Submit' is pressed, the description and image URL link is sent to the MessageServlet so it can create a new Message.
     out.println("<form method=\"POST\" action=\"/messages\">");
     out.println("<p>Here's the image you uploaded:</p>");
     out.println("<a href=\"" + imageUrl + "\">");
@@ -81,12 +95,9 @@ public class ImageUploaderServlet extends HttpServlet {
     out.println("<p>Here's the text you entered:</p>");
     out.println(message);
     out.println("<textarea name=\"text\" style=\"display:none;\">" + fullMessage + "</textarea>");
-
     out.println("<p>Is this info correct?</p>");
-
-    out.println("<input type=\"submit\" name=\"Yes\">");
+    out.println("<input type=\"submit\" name=\"Submit\">");
     out.println("</form>");
-
   }
 
 
