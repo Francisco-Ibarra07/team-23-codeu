de052c4e994ad45cce2b212f927a963009dee8ef
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-07-19 18:45:54 -0700

    Renamed servlet to LabelEditor Servlet

--- src/main/java/com/google/codeu/servlets/LabelEditorServlet.java
+++ src/main/java/com/google/codeu/servlets/LabelEditorServlet.java
@@ -17,46 +17,24 @@
 package com.google.codeu.servlets;
 
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Message;
-import com.google.gson.Gson;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.List;
-import java.util.regex.Pattern;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
 
-/** Handles fetching and saving {@link Message} instances. */
-@WebServlet("/entitymanager")
-public class EntityManagerServlet extends HttpServlet {
+@WebServlet("/label-editor")
+public class LabelEditorServlet extends HttpServlet {
 
   private Datastore datastore;
 
   @Override
   public void init() {
     datastore = new Datastore();
-  }
-
-  /**
-   * Responds with a JSON representation of {@link Message} data for a specific user. Responds with
-   * an empty array if the user is not provided.
-   */
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-    System.out.println("Request:");
-    System.out.println(request);
   }
 
   @Override
@@ -67,7 +45,7 @@ public class EntityManagerServlet extends HttpServlet {
     String messageId = array[0].substring(1);
     String userEmail = array[1].substring(0, array[1].length() - 1);
 
-    Entity targetMessage = datastore.getMessageEntity(userEmail, messageId);
+    Entity targetMessage = datastore.getSpecificMessageEntity(userEmail, messageId);
     Boolean originalValue = (Boolean) targetMessage.getProperty("isFulfilled");
     targetMessage.setProperty("isFulfilled", !originalValue);
 
