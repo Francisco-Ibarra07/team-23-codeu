094001df0cf01eb6d5a3e188df40b8d54f6a486c
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-07-19 17:39:18 -0700

    Added isFulfilled instance variable to the Message class

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -25,20 +25,22 @@ public class Message {
   private String user;
   private String text;
   private long timestamp;
+  private boolean isFulfilled;
 
   /**
    * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
    * random ID and uses the current system time for the creation time.
    */
   public Message(String user, String text) {
-    this(UUID.randomUUID(), user, text, System.currentTimeMillis());
+    this(UUID.randomUUID(), user, text, System.currentTimeMillis(), false);
   }
 
-  public Message(UUID id, String user, String text, long timestamp) {
+  public Message(UUID id, String user, String text, long timestamp, boolean isFulfilled) {
     this.id = id;
     this.user = user;
     this.text = text;
     this.timestamp = timestamp;
+    this.isFulfilled = isFulfilled;
   }
 
   public UUID getId() {
@@ -55,5 +57,9 @@ public class Message {
 
   public long getTimestamp() {
     return timestamp;
+  }
+
+  public Boolean getIsFulfilled() {
+    return isFulfilled;
   }
 }
